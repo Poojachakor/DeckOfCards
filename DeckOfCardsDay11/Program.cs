e630781f996bb7a2c4a4c9ec5b7d00b704ab41c7
Poojachakor <chakorpooja07@gmail.com>
2023-04-07 16:21:01 +0530

    Add: DeckOfCardsDay11

---
+++ DeckOfCardsDay11/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.Numerics;
+using System;
+
+namespace DeckOfCardsDay11
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DeckOfCards deck = new DeckOfCards();
+            deck.Shuffle();
+
+            string[,] cards = deck.DealCards();
+
+            // Print the Cards received by the 4 Players using 2D Array
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"Player {i + 1}: ");
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write($"{cards[i, j]}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+            
+        
+    }
+}
+
+
+        
+
+
+
+
+
+    
